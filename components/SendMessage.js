71ba06d70abf3e9a39c6b68c57129114a4cf53b9
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-11-22 09:24:26 +0530

    :art: redesigned the send message card

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -8,7 +8,6 @@ import Icon from "./Icon";
 import Image from "./Image";
 import Text from "./Text";
 import TimeStamp from "./TimeStamp";
-import Avatar from "./Avatar";
 
 const ScreenWidth = Dimensions.get("screen").width;
 
@@ -19,154 +18,11 @@ function SendMessage({
   mime_type = "image/jpg",
   message_datetime,
   onMessagePress,
-  post_details = {},
   topDate = null,
   read = false,
   showRead = false,
 }) {
   const { colors, dark } = useTheme();
-
-  const GiveFileMessage = () => {
-    let key = mime_type.slice(0, 5);
-
-    switch (key) {
-      case "image":
-        return (
-          <View>
-            <View style={styles.ImageBox}>
-              <Image
-                uri={preview_file}
-                resizeMode="cover"
-                style={styles.Image}
-                onPress={onMessagePress}
-              />
-              <TimeStamp
-                style={styles.TimeStamp}
-                time={Helper.GetTimeFromObject(message_datetime)}
-              />
-            </View>
-            {message ? (
-              <Text text={message} size={16} family="Inter" marginTop={5} />
-            ) : null}
-          </View>
-        );
-
-      case "video":
-        return (
-          <View>
-            <View style={styles.ImageBox}>
-              <Image
-                uri={preview_file}
-                resizeMode="cover"
-                style={styles.Image}
-                onPress={onMessagePress}
-              />
-              <TimeStamp
-                style={styles.TimeStamp}
-                time={Helper.GetTimeFromObject(message_datetime)}
-              />
-              <Icon
-                Name="AntDesign"
-                IconName="play"
-                style={styles.PLayICon}
-                color={ColorPallete.primary}
-                size={30}
-                onPress={onMessagePress}
-              />
-            </View>
-            {message ? (
-              <Text text={message} size={16} family="Inter" marginTop={5} />
-            ) : null}
-          </View>
-        );
-      default:
-        return null;
-    }
-  };
-
-  const GivePostMessage = () => {
-    return (
-      <View>
-        <View
-          style={{
-            flexDirection: "row",
-            marginBottom: 10,
-            alignItems: "center",
-          }}
-        >
-          <Avatar size={30} uri={post_details.user_details.ProfilePicture} />
-          <Text
-            text={post_details.user_details.Name}
-            size={16}
-            family="InterBold"
-            marginLeft={10}
-          />
-        </View>
-        <View style={styles.ImageBox}>
-          <Image
-            uri={post_details.preview_file}
-            resizeMode="cover"
-            style={styles.Image}
-            onPress={onMessagePress}
-          />
-        </View>
-        {post_details.caption ? (
-          <Text
-            // Show only first 100 characters
-            text={post_details.caption.slice(0, 100)}
-            size={16}
-            family="Inter"
-            marginTop={5}
-          />
-        ) : null}
-        <Text
-          text={Helper.GetTimeFromObject(message_datetime)}
-          size={13}
-          family="Inter"
-          marginTop={5}
-          style={{ textAlign: "right" }}
-        />
-      </View>
-    );
-  };
-
-  const GiveMessageBody = () => {
-    switch (message_type) {
-      case "text":
-        return (
-          <View>
-            <Text text={message} size={16} family="Inter" />
-            <Text
-              text={Helper.GetTimeFromObject(message_datetime)}
-              size={10}
-              family="Inter"
-              marginTop={5}
-              style={{ textAlign: "right" }}
-            />
-          </View>
-        );
-
-      case "file":
-        return <View>{GiveFileMessage()}</View>;
-
-      case "post":
-        return <View>{GivePostMessage()}</View>;
-
-      default:
-        return (
-          <View>
-            <Text text={message} size={16} family="Inter" />
-            <Text
-              text={Helper.GetTimeFromObject(message_datetime)}
-              size={10}
-              family="Inter"
-              marginTop={5}
-              style={{ textAlign: "right" }}
-            />
-          </View>
-        );
-    }
-  };
 
   return (
     <View style={styles.container}>
@@ -184,11 +40,51 @@ function SendMessage({
       <View style={{ alignItems: "flex-end" }}>
         <View
           style={{
-            backgroundColor: dark ? colors.background : "#EAEAEA",
             ...styles.messageBox,
+            backgroundColor: dark ? colors.background : "#EAEAEA",
           }}
         >
-          {GiveMessageBody()}
+          <View style={styles.MessageBox}>
+            {message_type === "file" ? (
+              <View style={styles.ImageBox}>
+                <Image
+                  uri={preview_file}
+                  resizeMode="cover"
+                  style={styles.Image}
+                  onPress={onMessagePress}
+                  borderRadius={10}
+                />
+                <TimeStamp
+                  style={styles.TimeStamp}
+                  time={Helper.GetTimeFromObject(message_datetime)}
+                />
+                {mime_type.slice(0, 5) === "video" ? (
+                  <Icon
+                    Name="AntDesign"
+                    IconName="play"
+                    style={styles.PLayICon}
+                    color={ColorPallete.primary}
+                    size={30}
+                    onPress={onMessagePress}
+                  />
+                ) : null}
+              </View>
+            ) : null}
+
+            {message ? <Text text={message} size={16} family="Inter" /> : null}
+
+            {message_type === "text" ? (
+              <View>
+                <Text
+                  text={Helper.GetTimeFromObject(message_datetime)}
+                  size={10}
+                  family="Inter"
+                  marginTop={5}
+                  style={{ textAlign: "right" }}
+                />
+              </View>
+            ) : null}
+          </View>
         </View>
       </View>
 
@@ -222,6 +118,7 @@ const styles = StyleSheet.create({
     maxWidth: ScreenWidth - 150,
     padding: 10,
     borderRadius: 10,
+    elevation: 10,
   },
   TimeStamp: {
     position: "absolute",
