37b653a2da88025c45db7001b24b4ca142e48461
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-11-23 10:51:12 +0530

    :hammer: moved NewPostMenuCard to components dir

---
+++ components/NewPostMenuCard.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { TouchableRipple } from "react-native-paper";
+
+import ColorPallete from "../config/ColorPallete";
+import Icon from "./Icon";
+import Text from "./Text";
+
+function NewPostMenuCard({ Name, IconName, onPress, text }) {
+  return (
+    <TouchableRipple
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: ColorPallete.primary,
+        margin: 10,
+        borderRadius: 10,
+        elevation: 5,
+      }}
+      onPress={onPress}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Icon
+          Name={Name}
+          IconName={IconName}
+          size={60}
+          onPress={onPress}
+          color={"white"}
+        />
+        <Text
+          text={text}
+          color={"white"}
+          size={20}
+          marginTop={15}
+          family="InterBold"
+        />
+      </View>
+    </TouchableRipple>
+  );
+}
+
+export default NewPostMenuCard;
+
+const styles = StyleSheet.create({
+  container: {},
+});
