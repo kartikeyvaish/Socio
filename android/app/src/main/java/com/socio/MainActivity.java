bc376d33580d97ee462392f16774706aa40e36d6
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-03-10 00:40:12 +0530

    :racehorse: created new splashscreens/icons

--- android/app/src/main/java/com/socio/MainActivity.java
+++ android/app/src/main/java/com/socio/MainActivity.java
@@ -1,20 +1,13 @@
 package com.socio;
 
-import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    getReactInstanceManager().onConfigurationChanged(this, newConfig);
-  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +18,10 @@ public class MainActivity extends ReactActivity {
     super.onCreate(null);
   }
 
+  /**
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
+   */
   @Override
   protected String getMainComponentName() {
     return "main";
@@ -34,12 +31,27 @@ public class MainActivity extends ReactActivity {
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegateWrapper(
       this,
-      new ReactActivityDelegate(this, getMainComponentName()) {
-        @Override
-        protected ReactRootView createRootView() {
-          return new RNGestureHandlerEnabledRootView(MainActivity.this);
-        }
-      }
+      new ReactActivityDelegate(this, getMainComponentName())
     );
+  }
+
+  /**
+   * Align the back button behavior with Android S
+   * where moving root activities to background instead of finishing activities.
+   * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+   */
+  @Override
+  public void invokeDefaultOnBackPressed() {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+      if (!moveTaskToBack(false)) {
+        // For non-root activities, use the default implementation to finish them.
+        super.invokeDefaultOnBackPressed();
+      }
+      return;
+    }
+
+    // Use the default back button implementation on Android S
+    // because it's doing more than {@link Activity#moveTaskToBack} in fact.
+    super.invokeDefaultOnBackPressed();
   }
 }
