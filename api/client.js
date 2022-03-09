4d705a12059c29dda3183227a2977aeaa0ef1a64
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-03-10 00:40:41 +0530

    :art: moved all APIs to a custom hook

--- api/client.js
+++
@@ -1,9 +0,0 @@
-import { create } from "apisauce";
-import config from "../config/config";
-
-const apiClient = create({
-  baseURL: config.URLs.BaseURL + config.URLs.apiVersion,
-  timeout: 15000,
-});
-
-export default apiClient;
