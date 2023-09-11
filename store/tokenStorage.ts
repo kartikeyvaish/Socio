1e6d757c010704d8cc0824ef8229935f5e2ba1d1
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-09-11 23:43:50 +0530

    :zap: created MMKV helpers

--- store/tokenStorage.ts
+++ store/tokenStorage.ts
@@ -3,15 +3,39 @@ import { MMKV } from "react-native-mmkv"
 
 const storage = new MMKV()
 
+export function getMMKVItem(key: string) {
+    return storage.getString(key)
+}
+
+export function setMMKVItem(key: string, value: string) {
+    storage.set(key, value)
+}
+
+export function removeMMKVItem(key: string) {
+    storage.delete(key)
+}
+
+export function setRefreshToken(token: string) {
+    setMMKVItem('refreshToken', token)
+}
+
+export function getRefreshToken() {
+    return getMMKVItem('refreshToken')
+}
+
+export function removeRefreshToken() {
+    removeMMKVItem('refreshToken')
+}
+
 export function setAccessToken(token: string) {
-    storage.set('accessToken', token)
+    setMMKVItem('accessToken', token)
 }
 
 export function getAccessToken() {
-    return storage.getString('accessToken')
+    return getMMKVItem('accessToken')
 }
 
 export function removeAccessToken() {
-    storage.delete('accessToken')
+    removeMMKVItem('accessToken')
 }
 
