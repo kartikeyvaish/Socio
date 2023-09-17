4ecf394aee15ae04656b33d44b0bfa30019cdf60
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-09-17 13:49:21 +0530

    :zap: added more variables in env types

--- react-native-config.d.ts
+++ react-native-config.d.ts
@@ -2,6 +2,7 @@ declare module 'react-native-config' {
     export interface NativeConfig {
         DEV_BASE_URL: string
         PROD_BASE_URL: string
+        GOOGLE_LOGIN_CLIENT_ID: string
     }
 
     export const Config: NativeConfig
