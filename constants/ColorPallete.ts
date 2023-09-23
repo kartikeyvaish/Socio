cae3accf8361ab76919a94ab96e49d02a6e48b0f
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-09-23 20:54:57 +0530

    :zap: added new color pallete

--- constants/ColorPallete.ts
+++ constants/ColorPallete.ts
@@ -1,14 +1,15 @@
 const ColorPallete = {
-    primary: '#1E90FF',
-    white: '#FFFFFF',
-    black: '#000000',
-    inputTextColorLight: '#262626',
-    inputTextColorDark: '#FFFFFF',
-    inputBackgroundColorLight: '#FAFAFA',
-    inputBackgroundColorDark: '#121212',
-    placeholderTextColorLight: 'rgba(0, 0, 0, 0.20)',
-    placeholderTextColorDark: 'rgba(255, 255, 255, 0.60)',
-    danger: "#FF4C58",
+  primary: "#1E90FF",
+  white: "#FFFFFF",
+  black: "#000000",
+  inputTextColorLight: "#262626",
+  inputTextColorDark: "#FFFFFF",
+  inputBackgroundColorLight: "#FAFAFA",
+  inputBackgroundColorDark: "#121212",
+  placeholderTextColorLight: "rgba(0, 0, 0, 0.20)",
+  placeholderTextColorDark: "rgba(255, 255, 255, 0.60)",
+  danger: "#FF4C58",
+  translucent: "rgba(0, 0, 0, 0.5)",
 };
 
 export default ColorPallete;
