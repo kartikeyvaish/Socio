6b63bf5937ee2735f6d003a9d3f958878d0bd244
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-09-23 20:57:12 +0530

    :zap: added more environment variables

--- react-native-config.d.ts
+++ react-native-config.d.ts
@@ -1,11 +1,15 @@
-declare module 'react-native-config' {
-    export interface NativeConfig {
-        DEV_BASE_URL: string
-        PROD_BASE_URL: string
-        GOOGLE_LOGIN_CLIENT_ID: string
-        GOOGLE_CLIENT_IOS_CLIENT_ID: string
-    }
+declare module "react-native-config" {
+  export interface NativeConfig {
+    NODE_ENV: "development" | "production";
+    DEV_BASE_URL: string;
+    PROD_BASE_URL: string;
+    GOOGLE_LOGIN_CLIENT_ID: string;
+    GOOGLE_CLIENT_IOS_CLIENT_ID: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_UPLOAD_PRESET: string;
+  }
 
-    export const Config: NativeConfig
-    export default Config
+  export const Config: NativeConfig;
+  export default Config;
 }
