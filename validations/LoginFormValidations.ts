68aea78c1e303782651ea2b66134c3156e77d248
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-09-27 20:43:11 +0530

    :zap: formatted login validation

--- validations/LoginFormValidations.ts
+++ validations/LoginFormValidations.ts
@@ -4,16 +4,16 @@ import { EMAIL_SCHEMA, PASSWORD_SCHEMA } from "../types/YupSchema";
 
 // validate schema for login form
 const validationSchema = () => {
-    return Yup.object().shape({
-        email: EMAIL_SCHEMA,
-        password: PASSWORD_SCHEMA,
-    });
+  return Yup.object().shape({
+    email: EMAIL_SCHEMA,
+    password: PASSWORD_SCHEMA,
+  });
 };
 
 // initial values for login form
 const initialValues = {
-    email: "",
-    password: "",
+  email: "",
+  password: "",
 };
 
 // exports
