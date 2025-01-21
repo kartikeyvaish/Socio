18952893f287a1560bf81f9cefb86035c937ad76
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-21 21:36:21 +0530

    add config: added colorPallete for the app

---
+++ src/constants/colorPallete.ts
@@ -0,0 +1,10 @@
+export default {
+  // App Colors
+  primary: '#1E90FF',
+  white: '#FFFFFF',
+  black: '#000000',
+  danger: '#FF4C58',
+  success: '#22bb33',
+  warning: '#FFB259',
+  grey: '#ACACAC'
+};
