c3f9951f29a030db1c9197d96b565fa6e334a36a
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-21 23:17:17 +0530

    refractored: auth reducer file

--- src/store/feature/authSlice.ts
+++ src/store/feature/authSlice.ts
@@ -35,7 +35,6 @@ export const authSlice = createSlice({
   }
 });
 
-// Action creators are generated for each case reducer function
 export const { setUser, reset, logout } = authSlice.actions;
 
 export default authSlice.reducer;
