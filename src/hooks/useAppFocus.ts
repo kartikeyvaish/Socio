a04573823ff743683736055f93550c6edf62a181
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-21 23:46:35 +0530

    added hook: created useAppFocus to maintain state of app's visibility on screen

---
+++ src/hooks/useAppFocus.ts
@@ -0,0 +1,24 @@
+// Packages Imports
+import { useEffect, useRef, useState } from 'react';
+import { AppState } from 'react-native';
+
+export default function useAppFocus() {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  return appStateVisible;
+}
