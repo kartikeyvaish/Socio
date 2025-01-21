0a1ed6a97a7dd5a63c32e542d883ebaac2187b3a
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-21 23:07:56 +0530

    added config: wrapped the app under redux store

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
 // Packages Imports
 import { StyleSheet, Text, View } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 // Local imports
 import ErrorBoundary from './src/components/ErrorBoundary/ErrorBoundary';
+
+// Named Imports
+import { persistor, store } from './src/store';
 
 export default function App() {
   const apiUrl = process.env.EXPO_PUBLIC_DEV_SERVER_BASE_URL;
 
   return (
     <ErrorBoundary>
-      <View style={styles.container}>
-        <Text>{apiUrl}</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <View style={styles.container}>
+            <Text>{apiUrl}</Text>
+          </View>
+        </PersistGate>
+      </Provider>
     </ErrorBoundary>
   );
 }
