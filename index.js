e2e28d57277c447cf7494e614cae7c18fefd443d
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-21 21:22:34 +0530

    completed entry point setup for index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import { registerRootComponent } from 'expo';
+// Packages Imports
+import "react-native-gesture-handler";
+import { AppRegistry } from "react-native";
 
-import App from './App';
+// Local Files/App/Components/Store import
+import App from "./App";;
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+// Headless Check for PushNotifications
+function HeadlessCheck({ isHeadless }) {
+  return isHeadless ? null : (
+    <App />
+  );
+}
+
+// registering the App
+AppRegistry.registerComponent("main", () => HeadlessCheck);
