455bf3a199dcd3cf0d18e08b16bc55e3a1dfe9d3
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-23 00:15:44 +0530

    added component: created App Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,55 @@
+// Packages Imports (from node_modules)
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+// Local Imports (components/types/utils)
+import AppText from '../AppText';
+import AnimatedView from '../AnimatedView';
+import colorPallete from '../../constants/colorPallete';
+
+// Named Imports
+import { variables } from '../../constants/ui';
+
+// interface for Button component
+export interface ButtonProps extends RectButtonProps {
+  label?: string;
+  loading?: boolean;
+}
+
+// functional component for Button
+function Button(props: ButtonProps) {
+  // Destructuring props
+  const { label, loading, ...restProps } = props;
+
+  // render
+  return (
+    <RectButton style={styles.container} {...restProps}>
+      {loading ? (
+        <AnimatedView>
+          <ActivityIndicator animating={loading} color={colorPallete.white} />
+        </AnimatedView>
+      ) : null}
+
+      <AnimatedView>
+        <AppText text={label} color={colorPallete.white} size={16} />
+      </AnimatedView>
+    </RectButton>
+  );
+}
+
+// exports
+export default Button;
+
+// styles for Button
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 48,
+    borderRadius: variables.borderRadius.small,
+    backgroundColor: colorPallete.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: variables.gap.small
+  }
+});
