e781c1096980d75a229427b59d73ba1dacdfa943
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-23 00:02:53 +0530

    added config: added ui constants

---
+++ src/constants/ui.ts
@@ -0,0 +1,27 @@
+export const variables = {
+  padding: {
+    small: 8,
+    medium: 16,
+    large: 24
+  },
+  borderRadius: {
+    small: 6,
+    medium: 12,
+    large: 24
+  },
+  gap: {
+    small: 8,
+    medium: 16,
+    large: 24
+  },
+  fontSize: {
+    small: 12,
+    medium: 14,
+    large: 16
+  },
+  margin: {
+    small: 8,
+    medium: 16,
+    large: 24
+  }
+};
