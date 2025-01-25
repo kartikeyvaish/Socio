92129060b080fc23db667d8645b80e9c3071aee9
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-25 23:20:01 +0530

    configured props for FlexView

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -2,15 +2,24 @@
 import AnimatedView from '../AnimatedView';
 
 // interface for Flex component
-import { FlexViewProps } from '../../types/components';
+import { AnimatedViewProps, FlexViewProps } from '../../types/components';
 
 // functional component for Flex
 function Flex(props: FlexViewProps) {
   // Destructuring props
   const { flex, align, justify, row, gap, style, ...restProps } = props;
 
+  const flexStyle: AnimatedViewProps['style'] = [
+    flex && { flex },
+    align && { alignItems: align },
+    justify && { justifyContent: justify },
+    row && { flexDirection: 'row' },
+    gap && { gap },
+    style
+  ];
+
   // render
-  return <AnimatedView {...restProps} />;
+  return <AnimatedView style={flexStyle} {...restProps} />;
 }
 
 // exports
