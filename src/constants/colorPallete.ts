07a97ddb5867245a9746abcaebfbdb869ffd0c85
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-25 23:20:33 +0530

    added colors to ColorPallete

--- src/constants/colorPallete.ts
+++ src/constants/colorPallete.ts
@@ -6,5 +6,8 @@ export default {
   danger: '#FF4C58',
   success: '#22bb33',
   warning: '#FFB259',
-  grey98: '#FAFAFA'
+  grey98: '#FAFAFA',
+  darkModeBlack: '#212121',
+  placeholderLight: 'rgba(0, 0, 0, 0.2)',
+  placeholderDark: 'rgba(255, 255, 255, 0.6)'
 };
