82bcf787e9db8a24c2b80e1aba2865440361bd30
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-25 23:53:30 +0530

    added props to FlexView

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -7,14 +7,24 @@ import { AnimatedViewProps, FlexViewProps } from '../../types/components';
 // functional component for Flex
 function Flex(props: FlexViewProps) {
   // Destructuring props
-  const { flex, align, justify, row, gap, style, ...restProps } = props;
+  const { flex, align, justify, row, gap, style, margins, ...restProps } = props;
+
+  // seperate margins
+  const seperateMargins = {
+    margin: margins?.all,
+    marginTop: margins?.top,
+    marginRight: margins?.right,
+    marginBottom: margins?.bottom,
+    marginLeft: margins?.left
+  };
 
   const flexStyle: AnimatedViewProps['style'] = [
     flex && { flex },
     align && { alignItems: align },
     justify && { justifyContent: justify },
-    row && { flexDirection: 'row' },
+    { flexDirection: row ? 'row' : 'column' },
     gap && { gap },
+    seperateMargins,
     style
   ];
 
