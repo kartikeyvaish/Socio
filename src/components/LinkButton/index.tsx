a4ed53865dc28dc79d7c0eb012ff965db05a76b2
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-01-25 23:20:23 +0530

    added component: created Link Button component

---
+++ src/components/LinkButton/index.tsx
@@ -0,0 +1,35 @@
+// Packages Imports (from node_modules)
+import { StyleSheet, TextProps } from 'react-native';
+import Animated from 'react-native-reanimated';
+
+// Local Imports (components/types/utils)
+import colorPallete from '../../constants/colorPallete';
+
+// interface for LinkButton component
+export interface LinkButtonProps extends TextProps {
+  label: string;
+  color?: string;
+}
+
+// functional component for LinkButton
+function LinkButton(props: LinkButtonProps) {
+  // Destructuring props
+  const { label, color = colorPallete.primary, style, ...restProps } = props;
+
+  // render
+  return (
+    <Animated.Text style={[{ color }, styles.textStyles, style]} {...restProps}>
+      {label}
+    </Animated.Text>
+  );
+}
+
+// exports
+export default LinkButton;
+
+// styles for LinkButton
+const styles = StyleSheet.create({
+  textStyles: {
+    fontWeight: '400'
+  }
+});
