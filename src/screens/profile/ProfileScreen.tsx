86a4afbc3658426b350249dba2dd048a90a8563a
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-02-05 10:33:00 +0530

    wired Profile screen

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,7 +1,8 @@
 // Packages Imports (from node_modules)
 import { useCallback, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { Image } from 'expo-image';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 // Local Imports (components/types/utils)
 import AppText from '../../components/AppText';
@@ -13,6 +14,7 @@ import PostMiniCard from '../../components/Post/PostMiniCard';
 import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 
 // Named Imports
+import { AppStackParamsList } from '../../navigation/types';
 import { DEFAULT_USER_IMAGE, fontFamilies } from '../../constants/ui';
 import { Profile, Post as PostType } from '../../types/model';
 import { useAppSelector } from '../../store/storeHooks';
@@ -43,13 +45,10 @@ function ProfileScreen(props: ProfileScreenProps) {
 
   const { user } = useAppSelector((state) => state.auth);
 
-  const [profilePosts, setProfilePosts] = useState<Array<PostType>>(SAMPLE_PROFILE_POSTS);
+  const navigation = useNavigation<NavigationProp<AppStackParamsList>>();
 
-  // const {
-  //   data: profilePosts,
-  //   getData: getMoreProfilePosts,
-  //   isFetching
-  // } = useInfiniteScroll<PostType>(getProfilePosts);
+  const { data: profilePosts, getData: getMoreProfilePosts } =
+    useInfiniteScroll<PostType>(getProfilePosts);
 
   // Local States
   const [profileDetails, setProfileDetails] = useState<Profile>(
@@ -77,6 +76,9 @@ function ProfileScreen(props: ProfileScreenProps) {
       <PostMiniCard
         {...post}
         position={index % 3 === 0 ? 'left' : index % 3 === 1 ? 'center' : 'right'}
+        onPress={() =>
+          navigation.navigate('PostListScreen', { posts: profilePosts, startIndex: index })
+        }
       />
     ),
     []
@@ -93,6 +95,8 @@ function ProfileScreen(props: ProfileScreenProps) {
         ListHeaderComponent={
           profileDetails ? <ProfileDetails profileDetails={profileDetails} /> : null
         }
+        onEndReached={getMoreProfilePosts}
+        onEndReachedThreshold={0.5}
         columnWrapperStyle={{ borderTopWidth: 1 - StyleSheet.hairlineWidth, borderColor: 'white' }}
       />
     </Container>
@@ -143,9 +147,10 @@ export default ProfileScreen;
 
 // styles for ProfileScreen
 const styles = StyleSheet.create({
-  container: {},
   contentContainer: {
-    padding: 8
+    padding: 8,
+    paddingLeft: 12,
+    paddingRight: 12
   },
   userProfileImageContainer: {
     width: 90,
@@ -153,355 +158,3 @@ const styles = StyleSheet.create({
     borderRadius: 45
   }
 });
-
-const SAMPLE_PROFILE_POSTS: Array<PostType> = [
-  {
-    id: 143,
-    location: 'East Joannie',
-    caption: 'Defleo tricesimus solio subseco supra alias volva abstergo allatus.',
-    is_edited: false,
-    is_archieved: false,
-    comments_enabled: true,
-    created_at: '2025-01-30T14:38:50.316906+00:00',
-    user: {
-      bio: 'Some Bio',
-      username: 'kartikey',
-      last_name: 'Vaish',
-      first_name: 'Kartikey',
-      profile_picture:
-        'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738392973/socio_assets/users_1/profile_picture/ujmlfrhjvy20leclpqn1.jpg'
-    },
-    files: [
-      {
-        id: 525,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'LtMa08jDxsbI?vW.adof~pn~R:jX',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/mock/mr9az1aqfybamzsrxuy2.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738136650/socio_assets/mock/mr9az1aqfybamzsrxuy2.mp4',
-        playback_url: null,
-        resource_type: 'video'
-      },
-      {
-        id: 526,
-        width: 4096,
-        format: 'mp4',
-        height: 2160,
-        blurhash: 'LCDwINm+4TIt}{x_yZx].B.9H]oz',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_2160,w_4096/v1/socio_assets/mock/ebotqwjutpiyzczjldcg.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738134776/socio_assets/mock/ebotqwjutpiyzczjldcg.mp4',
-        playback_url: null,
-        resource_type: 'video'
-      },
-      {
-        id: 527,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'L9B:57~V0KE1%2NGRjae004:~C%M',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/mock/mqubotqhevbnikepimmf.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738136724/socio_assets/mock/mqubotqhevbnikepimmf.mp4',
-        playback_url: null,
-        resource_type: 'video'
-      },
-      {
-        id: 528,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'LD9[I|a08}oz?@oIRQWE00o}.PV@',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/mock/nsgn2edvocztsd1z5etc.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738136630/socio_assets/mock/nsgn2edvocztsd1z5etc.mp4',
-        playback_url: null,
-        resource_type: 'video'
-      },
-      {
-        id: 529,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'L+NJ@1oJs:xa~qoMayae%fWXWVWC',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/mock/zpnlki1xesydn43ekkcw.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738135225/socio_assets/mock/zpnlki1xesydn43ekkcw.mp4',
-        playback_url: null,
-        resource_type: 'video'
-      }
-    ],
-    total_likes: 106,
-    is_liked: true,
-    total_comments: 7,
-    is_saved: false
-  },
-  {
-    id: 243,
-    location: ' London',
-    caption: ' WOW! What a day',
-    is_edited: false,
-    is_archieved: false,
-    comments_enabled: true,
-    created_at: '2025-02-01T11:35:20.593035+00:00',
-    user: {
-      bio: 'Some Bio',
-      username: 'kartikey',
-      last_name: 'Vaish',
-      first_name: 'Kartikey',
-      profile_picture:
-        'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738392973/socio_assets/users_1/profile_picture/ujmlfrhjvy20leclpqn1.jpg'
-    },
-    files: [
-      {
-        id: 746,
-        width: 1080,
-        format: 'jpg',
-        height: 1350,
-        blurhash: 'LBKSz[-U0MV?~Vxa9bIo01-;E1bE',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/gmcyf2zflbvcrezz92cq.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 747,
-        width: 2736,
-        format: 'jpg',
-        height: 1539,
-        blurhash: 'LAE,pgD,[n$z.T11]Os,7f1fJ+sA',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/ynwfu7ywgahbc27kgr53.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 748,
-        width: 720,
-        format: 'mp4',
-        height: 900,
-        blurhash: 'L26[jeFxL#%1={W;ODo200v#HWIp',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_900,w_720/v1/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.m3u8',
-        resource_type: 'video'
-      },
-      {
-        id: 749,
-        width: 1080,
-        format: 'jpg',
-        height: 1349,
-        blurhash: 'LBHKwl4.00?uHX%1%#9a=v~B%LE1',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/fqew4lvol9igrbq3ae2k.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 750,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'LMQc6q_N%zs+4-?vtRjF57%MI9WB',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.m3u8',
-        resource_type: 'video'
-      }
-    ],
-    total_likes: 0,
-    is_liked: false,
-    total_comments: 0,
-    is_saved: false
-  },
-  {
-    id: 24113,
-    location: ' London',
-    caption: ' WOW! What a day',
-    is_edited: false,
-    is_archieved: false,
-    comments_enabled: true,
-    created_at: '2025-02-01T11:35:20.593035+00:00',
-    user: {
-      bio: 'Some Bio',
-      username: 'kartikey',
-      last_name: 'Vaish',
-      first_name: 'Kartikey',
-      profile_picture:
-        'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738392973/socio_assets/users_1/profile_picture/ujmlfrhjvy20leclpqn1.jpg'
-    },
-    files: [
-      {
-        id: 746,
-        width: 1080,
-        format: 'jpg',
-        height: 1350,
-        blurhash: 'LBKSz[-U0MV?~Vxa9bIo01-;E1bE',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/gmcyf2zflbvcrezz92cq.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 747,
-        width: 2736,
-        format: 'jpg',
-        height: 1539,
-        blurhash: 'LAE,pgD,[n$z.T11]Os,7f1fJ+sA',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/ynwfu7ywgahbc27kgr53.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 748,
-        width: 720,
-        format: 'mp4',
-        height: 900,
-        blurhash: 'L26[jeFxL#%1={W;ODo200v#HWIp',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_900,w_720/v1/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.m3u8',
-        resource_type: 'video'
-      },
-      {
-        id: 749,
-        width: 1080,
-        format: 'jpg',
-        height: 1349,
-        blurhash: 'LBHKwl4.00?uHX%1%#9a=v~B%LE1',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/fqew4lvol9igrbq3ae2k.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 750,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'LMQc6q_N%zs+4-?vtRjF57%MI9WB',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.m3u8',
-        resource_type: 'video'
-      }
-    ],
-    total_likes: 0,
-    is_liked: false,
-    total_comments: 0,
-    is_saved: false
-  },
-  {
-    id: 2243,
-    location: ' London',
-    caption: ' WOW! What a day',
-    is_edited: false,
-    is_archieved: false,
-    comments_enabled: true,
-    created_at: '2025-02-01T11:35:20.593035+00:00',
-    user: {
-      bio: 'Some Bio',
-      username: 'kartikey',
-      last_name: 'Vaish',
-      first_name: 'Kartikey',
-      profile_picture:
-        'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738392973/socio_assets/users_1/profile_picture/ujmlfrhjvy20leclpqn1.jpg'
-    },
-    files: [
-      {
-        id: 746,
-        width: 1080,
-        format: 'jpg',
-        height: 1350,
-        blurhash: 'LBKSz[-U0MV?~Vxa9bIo01-;E1bE',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/gmcyf2zflbvcrezz92cq.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 747,
-        width: 2736,
-        format: 'jpg',
-        height: 1539,
-        blurhash: 'LAE,pgD,[n$z.T11]Os,7f1fJ+sA',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/ynwfu7ywgahbc27kgr53.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 748,
-        width: 720,
-        format: 'mp4',
-        height: 900,
-        blurhash: 'L26[jeFxL#%1={W;ODo200v#HWIp',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_900,w_720/v1/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409723/socio_assets/users_1/posts_243/cxifezhzerehukm5br3r.m3u8',
-        resource_type: 'video'
-      },
-      {
-        id: 749,
-        width: 1080,
-        format: 'jpg',
-        height: 1349,
-        blurhash: 'LBHKwl4.00?uHX%1%#9a=v~B%LE1',
-        thumbnail: null,
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/image/upload/v1738409722/socio_assets/users_1/posts_243/fqew4lvol9igrbq3ae2k.jpg',
-        playback_url: '',
-        resource_type: 'image'
-      },
-      {
-        id: 750,
-        width: 720,
-        format: 'mp4',
-        height: 1280,
-        blurhash: 'LMQc6q_N%zs+4-?vtRjF57%MI9WB',
-        thumbnail:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/c_thumb,h_1280,w_720/v1/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.jpg?_a=BAMCkGUq0',
-        secure_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.mp4',
-        playback_url:
-          'https://res.cloudinary.com/kartikeyvaish/video/upload/sp_auto/v1738409724/socio_assets/users_1/posts_243/gnyuthwicmv3g00gnzty.m3u8',
-        resource_type: 'video'
-      }
-    ],
-    total_likes: 0,
-    is_liked: false,
-    total_comments: 0,
-    is_saved: false
-  }
-];
