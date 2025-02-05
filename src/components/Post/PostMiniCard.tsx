1685cf64a312ebdf7398172a0fa9e2bacad4127c
Kartikey Vaish <kartikeyvaish99@gmail.com>
2025-02-05 09:58:52 +0530

    created boiler plate Post mini card

---
+++ src/components/Post/PostMiniCard.tsx
@@ -0,0 +1,67 @@
+// Packages Imports (from node_modules)
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+// Local Imports (components/types/utils)
+
+// Named Imports
+import { Post } from '../../types/model';
+import { SCREEN_WIDTH } from '../../constants/ui';
+import { Image } from 'expo-image';
+
+// interface for PostMiniCard component
+export interface PostMiniCardProps extends Post {
+  position: 'left' | 'right' | 'center';
+}
+
+// functional component for PostMiniCard
+function PostMiniCard(props: PostMiniCardProps) {
+  // Destructuring props
+  const { position, files } = props;
+
+  let cardStyles =
+    position === 'center' ? styles.centerCard : position === 'left' ? styles.leftCard : {};
+
+  let hasMultipleFiles = files.length > 1;
+
+  let firstFile = files[0];
+
+  if (!firstFile) return null;
+
+  // render
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={[styles.container, cardStyles]}>
+      <Image
+        placeholder={{ blurhash: firstFile.blurhash }}
+        style={styles.image}
+        contentFit="cover"
+      />
+    </TouchableOpacity>
+  );
+}
+
+// exports
+export default PostMiniCard;
+
+// styles for PostMiniCard
+const styles = StyleSheet.create({
+  container: {
+    width: SCREEN_WIDTH / 3,
+    height: SCREEN_WIDTH / 3,
+    backgroundColor: 'pink',
+    borderBottomWidth: 1 - StyleSheet.hairlineWidth,
+    borderColor: 'white'
+  },
+  centerCard: {
+    borderRightWidth: 1 - StyleSheet.hairlineWidth,
+    borderColor: 'white'
+  },
+  leftCard: {
+    borderRightWidth: 1 - StyleSheet.hairlineWidth,
+    borderColor: 'white'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
+  }
+});
